dee21d3cb6dc487a27d0e15689c57ba25451fb69
vikascowdary47 <vikaschowdary47@gmail.com>
2020-08-20 15:32:06 +0530

    fizzBuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,16 @@
+const fizzBuzz = () => {
+  for (let i = 0; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+};
+
+const output = fizzBuzz();
+console.log(output);
